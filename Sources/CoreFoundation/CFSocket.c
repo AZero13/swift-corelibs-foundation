b3c85eaf158874dea9771c57c036e189ada0b40c
AZero13 <gfunni234@gmail.com>
2026-01-17 00:44:33 -0500

    Update CFSocket.c

--- Sources/CoreFoundation/CFSocket.c
+++ Sources/CoreFoundation/CFSocket.c
@@ -1404,8 +1404,7 @@ static void *__CFSocketManager(void * arg)
         }
 
         if (exceptfds == NULL) {
-            exceptfds = (fd_set*) malloc(maxnrfds * NFDBITS);
-            bzero(exceptfds, maxnrfds * NFDBITS);
+            exceptfds = (fd_set*) calloc(maxnrfds, NFDBITS);
         }
 #endif
 
